7a80b9895236b7feb9f4fbdad3c24c12bd502a33
sakakibara88 <61224740+sakakibara88@users.noreply.github.com>
2020-03-11 21:04:32 +0900

    イイね機能の並び替え実装

--- app/controllers/goods_controller.rb
+++ app/controllers/goods_controller.rb
@@ -3,25 +3,19 @@ class GoodsController < ApplicationController
 
   def create
     @good = current_user.goods.create(content_id: params[:content_id])
-    @contents = @item.contents.includes(:user)
+    @contents        = @item.contents.includes(:user).joins(:goods).group(:content_id).order('count(content_id) DESC')
+    @contents_ungood = @item.contents.includes(:user)
     @items    = Item.where(items: {title_id: [@title]})
     @goods    = Good.where(content_id: @contents)
-
-    # @content  = Content.new
-    # @titles   = Title.all
-    # @good     = Good.new
   end
 
   def destroy
     @good = Good.find_by(content_id: params[:content_id], user_id: current_user.id)
     @good.destroy
-    @contents = @item.contents.includes(:user)
+    @contents        = @item.contents.includes(:user).joins(:goods).group(:content_id).order('count(content_id) DESC')
+    @contents_ungood = @item.contents.includes(:user)
     @items    = Item.where(items: {title_id: [@title]})
     @goods    = Good.where(content_id: @contents)
-
-    @content  = Content.new
-    @titles   = Title.all
-    @good     = Good.new
   end
   
   private
