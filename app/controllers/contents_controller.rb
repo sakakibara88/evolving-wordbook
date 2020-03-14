ab3614ac58d6da7537f1c8169bb92dcc93bc0212
sakakibara88 <61224740+sakakibara88@users.noreply.github.com>
2020-03-14 22:52:44 +0900

    Update contents_controller.rb

--- app/controllers/contents_controller.rb
+++ app/controllers/contents_controller.rb
@@ -5,7 +5,7 @@ class ContentsController < ApplicationController
     @content         = Content.new
     @titles          = Title.all
     @items           = Item.where(items: {title_id: [@title]})
-    @contents        = @item.contents.includes(:user).joins(:goods).group(:content_id).order('count(content_id) DESC')
+    @contents        = @item.contents.includes(:user)
     @contents_ungood = @item.contents.includes(:user)
     @goods           = Good.where(content_id: @contents)
   end
