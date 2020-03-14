1dde7f6e81b95e17e4b200216a3988568f31230d
sakakibara88 <61224740+sakakibara88@users.noreply.github.com>
2020-03-14 23:05:42 +0900

    Update goods_controller.rb

--- app/controllers/goods_controller.rb
+++ app/controllers/goods_controller.rb
@@ -3,7 +3,7 @@ class GoodsController < ApplicationController
 
   def create
     @good            = current_user.goods.create(content_id: params[:content_id])
-    @contents        = @item.contents.includes(:user).joins(:goods).group(:content_id).order('count(content_id) DESC')
+    @contents        = @item.contents.includes(:user).joins(:goods).group(:id).order('count(content_id) DESC')
     @contents_ungood = @item.contents.includes(:user)
     @items           = Item.where(items: {title_id: [@title]})
     @goods           = Good.where(content_id: @contents)
